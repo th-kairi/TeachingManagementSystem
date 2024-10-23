cc02a459528ee8eb28496cc69fb9fad733f77989
h_tsujimoto <h_tsujimoto@h-tsujimoto>
2024-10-23 17:19:23 +0900

    不要な処理を削除

--- src/dao/AttendanceManagementDAO.java
+++ src/dao/AttendanceManagementDAO.java
@@ -51,17 +51,12 @@ public class AttendanceManagementDAO extends DAO {
 	public List<Attendance> getAttendancesByClassCd(String classCd, int year, int month) throws Exception {
 		List<Attendance> attendanceList = new ArrayList<>();
 
-		// yとmを元にLocalDateを作成
-		String y = "2024";
-		String m = "10";
-
 		// 月の最初の日を取得
 		LocalDate firstDay = LocalDate.of(year, month, 1);
 		LocalDate lastDay = firstDay.withDayOfMonth(firstDay.lengthOfMonth());
 
 		Date sqlFirstDate = Date.valueOf(firstDay);
 		Date sqlLastDate = Date.valueOf(lastDay);
-		System.out.println(sqlFirstDate);
 
 		String sql = "SELECT"
 				+ "    tbl1.studentid, tbl1.date, attendance.attendance, attendance.atreason, attendance.point "
